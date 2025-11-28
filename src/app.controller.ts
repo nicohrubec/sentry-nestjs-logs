470960c7b11f462a3d6150777756fc99edf5b6e7
Nicolas Hrubec <nico.hrubec@sentry.io>
2025-11-28 12:48:12 +0100

    log with sentry.logger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as Sentry from '@sentry/nestjs';
 
 @Controller()
 export class AppController {
@@ -7,11 +8,13 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    Sentry.logger.info('Hello World!');
     return this.appService.getHello();
   }
 
   @Get('error')
   getError(): string {
+    Sentry.logger.error('Test error!');
     throw new Error('Test error');
   }
 }
--- src/instrument.ts
+++ src/instrument.ts
@@ -3,6 +3,9 @@ import * as Sentry from "@sentry/nestjs";
 // Ensure to call this before requiring any other modules!
 Sentry.init({
   dsn: "https://c2366a90b36c04ff73ec58dc97ee782e@o1.ingest.us.sentry.io/4510442378887168",
+
+  enableLogs: true,
+
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for tracing.
   // We recommend adjusting this value in production
